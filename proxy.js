575838e2b9d8c6590f6d8557e644ec5b41ee216f
YousofSayed <yousef.sayed1231@gmail.com>
2023-08-18 12:18:49 -0700

    edit6

--- proxy.js
+++ proxy.js
@@ -1,9 +1,19 @@
 import express from 'express';
-
+import cors from 'cors';
 const app = express(), PORT = process.env.PORT || 7070;
 const tafseerEndpoint = `http://api.quran-tafseer.com/tafseer`
 
-app.get('/getTafseer/:id/:surah/:ahya/', async (req, res) => {
+
+const allowedOrigins = ['https://localhost:5173', 'https://rowaq.pages.dev'];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
+app.get('/getTafseer/:id/:surah/:ahya', async (req, res) => {
     try {
         const { id, surah, ahya } = req.params;
         const data = await (await fetch(`${tafseerEndpoint}/${id}/${surah}/${ahya}`, { method: 'GET' })).json()
